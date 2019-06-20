9a8a332c9d993cd046df11cc3a50755ccdce755e
IanGrubb <yangrubb@gmail.com>
2019-06-20 11:09:25 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,16 @@
 def alphabetize(arr)
   # code here
+
+  esp_alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  esp_array = esp_alpha.split("")
+  arr.sort_by do |word|
+    smash_word = word.split(" ").join("")
+    
+    loc_array = []
+    smash_word.split("").each do |letter|
+      loc_array.push(esp_alpha.index(letter))
+    end
+    loc_array
+  end
+
 end
