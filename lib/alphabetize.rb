52e40f4f3a20e4196aebe52b23abb47282285d10
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-21 14:40:45 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
   # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
+  alphabetize = arr.sort_by do |word|
+    word.split('').map do |letter|
+      esperanto_alphabet.index(letter)
+    end
+  end
 end
