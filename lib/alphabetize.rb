2154caddae6a54675d39a7387987e17af760de1f
thenatural86 <philip.smalls86@gmail.com>
2019-06-23 13:52:51 -0400

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+#use sort_by method as indicated in instructions
+  arr.sort_by do |string|
+
+#use split to turn string into array, and use .map to iterate each index(letter) and return new array
+    string.split('').map do |letter|
+
+#checks the index of the letter in alphabet, then sorts the first number of each string
+      ESPERANTO_ALPHABET.index(letter)
+    end
+  end
 end
